058c048ddadb21164c06fdb7adfc66b38a6ee6c5
SKALSKI Alan <AlanSkalski@outlook.fr>
2024-04-09 17:59:34 +0200

    edit et new routes, sécurité pr edit et new

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,42 @@
 class CarsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :new]
 
   def index
+    @cars = Car.all
   end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = current_user.cars.build(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @car = current_user.cars.find(params[:id])
+    redirect_to root_path, notice: "Access denied" if @car.nil?
+  end
+
+
+
+
+
+  private
+
+  def car_params
+    params.require(:car).permit(:model, :brand, :adress, :photo, :year_of_production, :price_per_day)
+  end
+
 
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
 
   validates :brand, presence: true
   validates :model, presence: true
   validates :adress, presence: true
+  validates :photo, presence: true
   validates :price_per_day, numericality: { greater_than: 0 }
 
 end
